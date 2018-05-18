1db228e825caf95b3fde2a12ca636d7aa295aab8
Thorsten Walk <darkiop@gmail.com>
2018-05-18 23:53:38 +0200

    version 0.0.1

--- isg_set.sh
+++ isg_set.sh
@@ -4,27 +4,10 @@
 #
 # call with 'isg_set_bash.sh parameter wert'
 #
-# https://forum.iobroker.net/viewtopic.php?t=5835
-# http://www.mit.edu/afs.new/sipb/user/ssen/src/curl-7.11.1/docs/curl.html
-#
-# curl $isg_website/save.php \
-#  -H "Host: isg" \
-#  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0" \
-#  -H "Accept: text/plain, */*; q=0.01" \
-#  -H "Accept-Language: de,en-US;q=0.7,en;q=0.3" \
-#  -H "Referer: http://isg/" \
-#  -H "Content-Type: application/x-www-form-urlencoded" \
-#  -H "X-Requested-With: XMLHttpRequest" \
-#  -H "Connection: keep-alive" \
-#  -H "Pragma: no-cache" \
-#  -H "Cache-Control: no-cache" \
-#  --compressed \
-#  --data "make=send" \
-#  --cookie-jar "cookie.txt"
-#  --data-urlencode "data=[{\"name\":\"val82\",\"value\":\"3\"}]"
+# Thorsten Walk (April 2018)
 
 # URL
-isg_website="http://isg"
+isgIP=$1
 
 # Keine Parameter uebergeben
 if [ $# -eq  0 ]
@@ -34,55 +17,17 @@ if [ $# -eq  0 ]
 fi
 
 # Zuviele Parameter uebergeben
-if [ $# -gt 2 ]
+if [ $# -gt 3 ]
   then
     echo "Zu viele Parameter, Ende"
     exit
 fi
 
-# https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
-function contains() {
-  local n=$#
-  local value=${!n}
-  for ((i=1;i < $#;i++)) {
-    if [ "${!i}" == "${value}" ]; then
-      echo "y"
-      return 0
-    fi
-  }
-  echo "n"
-  return 1
-}
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# TODO CHECK $1 = korrekter Parameter, $2 valider Wert
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# Parameter Check
-#first_parameter=("val39s" "val82" "val5" "val17")
-#second_parameter=("0" "1" "2" "3")
-
-#A=("one" "two" "three four")
-
-#if [ $(contains "${A[@]}" "one") == "y" ]; then
-#    echo "contains one"
-#fi
-
-#if [ $(contains "${A[@]}" "three") == "y" ]; then
-#    echo "contains three"
-#fi
-
-#if [ $? -eq 0 ]; then
-    #echo OK
-#else
-    #echo FAIL
-#fi
-
 # WERTE UBER ISG AENDERN
 # para1 = Parameter
 # para2 = Wert
 function set_isg_para () {
-  curl $isg_website/save.php \
+  curl http://$isgIP/save.php \
   -H "Host: isg" \
   -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0" \
   -H "Accept: text/plain, */*; q=0.01" \
@@ -99,7 +44,7 @@ function set_isg_para () {
   --data-urlencode "data=[{\"name\":\"$1\",\"value\":\"$2\"}]"
 }
 
-case "$1" in
+case "$2" in
   
   BETRIEBSART)
   # val39s
@@ -107,7 +52,7 @@ case "$1" in
   # 11 = AUTOMATIK, 1 = BEREITSCHAFT, 3 = TAGBETRIEB, 4 = ABSENKBETRIEB,
   # 5 = WARMWASSER, 14 = HANDBETRIEB, 0 = NOTBETRIEB
   para=val39s
-  case "$2" in
+  case "$3" in
     AUTOMATIK)
     value=11
     ;;
@@ -140,7 +85,7 @@ case "$1" in
   # Gültige Werte:
   # 0,1,2,3
   para=val82
-  value=$2
+  value=$3
   echo "setze LUEFTERSTUFETAG"
   echo "auf WERT: $value"
   set_isg_para $para $value
@@ -152,7 +97,7 @@ case "$1" in
   # Gültige Werte:
   # 10.0 bis 30.0
   para=val5
-  value=$2
+  value=$3
   echo "setze RAUMTEMPTAG ($para) auf WERT: $value"
   set_isg_para $para $value
   exit
@@ -163,7 +108,7 @@ case "$1" in
   # Gültige Werte:
   # 10.0 bis 55.0
   para=val17
-  value=$2
+  value=$3
   echo "setze WARMWASSERTEMPTAG ($para) auf WERT: $value"
   set_isg_para $para $value
   exit
---
+++ stiebel-lwz.js
@@ -0,0 +1,90 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+'use strict';
+
+// https://nodejs.org/api/child_process.html
+const { exec } = require('child_process');
+var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var adapter = new utils.Adapter('stiebel-lwz');
+
+// is called when adapter shuts down - callback has to be called under any circumstances!
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+// is called if a subscribed object changes
+adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+// is called if a subscribed state changes
+adapter.on('stateChange', function (id, state) {
+
+    // Warning, state can be null if it was deleted
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+
+    var parameter, wert;
+    function set_isg_para(isgweburl, parameter, wert) {
+        exec('bash /opt/iobroker/node_modules/iobroker.stiebel-lwz/isg_set.sh ' + isgweburl + ' ' + parameter + ' ' + wert, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+        });
+    }
+
+    // TODO Werte validieren
+    if (id == adapter.name + '.' + adapter.instance + '.ISG_SET_BETRIEBSART') {
+        adapter.log.info('Setze ISG_SET_BETRIEBSART auf: ' + state.val);
+        set_isg_para(adapter.config.isgIP, 'BETRIEBSART', state.val);
+    } else if (id == adapter.name + '.' + adapter.instance + '.ISG_SET_LUEFTERSTUFETAG') {
+        adapter.log.info('Setze ISG_SET_LUEFTERSTUFETAG auf: ' + state.val);
+        set_isg_para(adapter.config.isgIP, 'LUEFTERSTUFETAG', state.val);
+    } else if (id == adapter.name + '.' + adapter.instance + '.ISG_SET_RAUMTEMPTAG') {
+        adapter.log.info('Setze ISG_SET_RAUMTEMPTAG auf: ' + state.val);
+        set_isg_para(adapter.config.isgIP, 'RAUMTEMPTAG', state.val);
+    } else if (id == adapter.name + '.' + adapter.instance + '.ISG_SET_WARMWASSERTEMPTAG') {
+        adapter.log.info('Setze ISG_SET_WARMWASSERTEMPTAG auf: ' + state.val);
+        set_isg_para(adapter.config.isgIP, 'WARMWASSERTEMPTAG', state.val);
+    }
+
+});
+
+// is called when databases are connected and adapter received configuration.
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+
+    adapter.log.info('isgIP: ' + adapter.config.isgIP);
+
+    var isgParameters = ['ISG_SET_BETRIEBSART', 'ISG_SET_LUEFTERSTUFETAG', 'ISG_SET_RAUMTEMPTAG', 'ISG_SET_WARMWASSERTEMPTAG'];
+
+    for (var key in isgParameters) {
+        
+        var obj = isgParameters[key];
+        
+        adapter.setObjectNotExists(isgParameters[key], {
+            type: 'state',
+            common: {
+                name: isgParameters[key],
+                type: 'string',
+                role: 'value'
+            },
+            native: {}
+        });
+
+      }
+       
+    // in this stiebel-lwz all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('*');
+
+}
