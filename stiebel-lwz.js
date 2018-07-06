bfd59204305b128cff1f5f9fd25bbccab1bd4303
Thorsten Walk <darkiop@gmail.com>
2018-07-06 19:48:19 +0200

    load betriebsart on adapter start - test

--- stiebel-lwz.js
+++ stiebel-lwz.js
@@ -4,6 +4,8 @@
 
 // https://nodejs.org/api/child_process.html
 const { exec } = require('child_process');
+
+
 var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var adapter = new utils.Adapter('stiebel-lwz');
 
@@ -29,6 +31,7 @@ adapter.on('stateChange', function (id, state) {
     // Warning, state can be null if it was deleted
     adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
+    // Sende Parameter an ISGweb
     var parameter, wert;
     function set_isg_para(isgweburl, parameter, wert) {
         exec('bash /opt/iobroker/node_modules/iobroker.stiebel-lwz/isg_set.sh ' + isgweburl + ' ' + parameter + ' ' + wert, (err, stdout, stderr) => {
@@ -40,17 +43,23 @@ adapter.on('stateChange', function (id, state) {
         });
     }
 
-    // TODO Werte validieren
+    // BETRIEBSART
     if (id == adapter.name + '.' + adapter.instance + '.Start.Betriebsart') {
         adapter.log.info('Setze Betriebsart auf: ' + state.val);
         set_isg_para(adapter.config.isgIP, 'BETRIEBSART', state.val);
-    } else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Lueften.Lueftungsstufen.STUFE_TAG') {
+    }
+    // LUEFTUNGSSTUFEN
+    else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Lueften.Lueftungsstufen.STUFE_TAG') {
         adapter.log.info('Setze Lueftungsstufe STUFE-TAG auf: ' + state.val);
         set_isg_para(adapter.config.isgIP, 'LUEFTERSTUFETAG', state.val);
-    } else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Heizen.Raumtemperaturen_HK1.RAUMTEMP_TAG') {
+    }
+    // RAUM TEMP TAG
+    else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Heizen.Raumtemperaturen_HK1.RAUMTEMP_TAG') {
         adapter.log.info('Setze Raumtemperaturen_HK1 RAUMTEMP. TAG auf: ' + state.val);
         set_isg_para(adapter.config.isgIP, 'RAUMTEMPTAG', state.val);
-    } else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Warmwasser.WW-Temperaturen.WW_SOLL_TAG') {
+    }
+    // WARMWASSER TEMP TAG
+    else if (id == adapter.name + '.' + adapter.instance + '.Einstellungen.Warmwasser.WW-Temperaturen.WW_SOLL_TAG') {
         adapter.log.info('Setze WW-Temperaturen WW SOLL TAG auf: ' + state.val);
         set_isg_para(adapter.config.isgIP, 'WARMWASSERTEMPTAG', state.val);
     }
@@ -64,9 +73,26 @@ adapter.on('ready', function () {
 
 function main() {
 
+    // Ausgabe der eingestellten IP im Log
     adapter.log.info('isgIP: ' + adapter.config.isgIP);
-      
+    
+    // https://github.com/IonicaBizau/cheerio-req    
+    const cheerioReq = require("cheerio-req");
+
+    // Lade aktuelle Betriebsart bei Start in Objekt
+    function load_isg_parameters() {
+
+        // BETRIEBSART
+        cheerioReq('http://' + adapter.config.isgIP, (err, $) => {
+            var betriebsart = $('input#aval39').val();
+            adapter.setState('Start.Betriebsart', betriebsart, true);
+            adapter.log.info('Aktuelle Betriebsart:' + betriebsart);
+        });
+
+    }
+    load_isg_parameters();
+
     // in this stiebel-lwz all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 
-}
+} // end of main()
